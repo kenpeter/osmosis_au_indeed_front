fae55c8811d64b4964ff40fc46d305a720543fc0
vpsdime <vpsdime@vpsdime.com>
2017-07-22 14:11:40 +0000

    [up] up

--- src/components/HeaderBar.vue
+++ src/components/HeaderBar.vue
@@ -1,7 +1,7 @@
 <!-- / or /cart -->
 <template>
   <div class="menu-links">
-		<p>This site scrapes php, javascript, web, it jobs from <a href="https://indeed.com">https://indeed.com</a> every night. It makes easy to navgate jobs in one long page.</p>
+		<p>This site scrapes php, javascript, web, it jobs from <a href="https://au.indeed.com/">https://au.indeed.com/</a> every night. It makes easy to navgate jobs in one long page.</p>
 		<p>Hire me at: <a href="https://au.linkedin.com/in/thegaryliang">https://au.linkedin.com/in/thegaryliang</a></p>
   </div>
 </template>
