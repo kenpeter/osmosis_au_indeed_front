8ea50d8656534a88ab05e5f2035317d0bb84b368
vpsdime <vpsdime@vpsdime.com>
2017-07-22 13:53:58 +0000

    [up] up

--- src/components/HeaderBar.vue
+++ src/components/HeaderBar.vue
@@ -1,7 +1,8 @@
 <!-- / or /cart -->
 <template>
   <div class="menu-links">
-    <a v-link="{path:'/'}">Job from au indeed</a>
+		<p>This site scrapes php, javascript, web, it jobs from <a href="https://indeed.com">https://indeed.com</a> every night. It makes easy to navgate jobs in one long page.</p>
+		<p>Hire me at: <a href="https://au.linkedin.com/in/thegaryliang">https://au.linkedin.com/in/thegaryliang</a></p>
   </div>
 </template>
 
